7ebbe24b0f19f117ac183cf6d780c4d360a1268e
priyanshh-09 <priyanshuchandola9@gmail.com>
2026-01-27 11:10:15 +0530

    Created controllers for updateProblem,deleteProblem,fetchProblem and fetchAllProblem

--- src/controllers/userProblems.js
+++ src/controllers/userProblems.js
@@ -43,7 +43,7 @@ const createproblem = async(req,res)=>{
 
                 const resToken = submitResult.map((val) => val.token);
                 const testRes = await submitToken(resToken);
-                console.log(testRes);
+                // console.log(testRes);
 
                 for (const test of testRes) {
                 if (test.status_id != 3) {
@@ -69,4 +69,117 @@ const createproblem = async(req,res)=>{
     }
 }
 
-module.exports = createproblem;
+const updateproblem = async(req,res)=>{
+     const {
+       title,
+       description,
+       difficulty,
+       tags,
+       visibleTestCases,
+       invisibleTestCases,
+       startcode,
+       referenceSolution,
+       // problemCreator,
+     } = req.body;
+     const{id} = req.params;
+
+    try{
+         
+        if(!id){
+          return res.status(400).send("id is not valid")
+        }
+        
+        const dsaProblem = await Problem.findById(id);
+        if(!dsaProblem){
+          return res.status(404).send("No problem Associated with given id")
+        }
+
+        for (const { language, completeCode } of referenceSolution) {
+          const langId = getLangId(language);
+
+          if (!langId) {
+            return res.status(400).send(`Unsupported language: ${language}`);
+          }
+
+          const submissions = visibleTestCases.map((testcase) => ({
+            source_code: completeCode,
+            language_id: langId,
+            stdin: testcase.input,
+            expected_output: testcase.output,
+          }));
+
+          const submitResult = await submitBatch(submissions);
+          // console.log(submitResult);
+
+          const resToken = submitResult.map((val) => val.token);
+          const testRes = await submitToken(resToken);
+          // console.log(testRes);
+
+          for (const test of testRes) {
+            if (test.status_id != 3) {
+              return res.status(400).send("Error");
+            }
+          }
+        }
+
+        const newProblem = await Problem.findByIdAndUpdate(id,{...req.body},{runValidators:true},{new:true});
+         res.status(200).send("Successfully Updated",newProblem)
+    }catch(err){
+        res.status(500).send("Error :"+err);
+     }
+}
+
+const deleteproblem = async(req,res)=>{
+   const{id} = req.params;
+   try{
+      if (!id) {
+        return res.status(400).send("id is not valid");
+      }
+
+      const deletedProblem = await Problem.findByIdAndDelete(id);
+      if(!deletedProblem){
+        return res.status(404).send("Problem is Missing");
+      }
+      res.status(200).send("Succesfully deleted",deletedProblem)
+   }catch(err){
+        res.status(500).send("Error :" + err);
+     
+   }
+}
+
+const fetchproblem = async(req,res)=>{
+   const { id } = req.params;
+   try {
+     if (!id) {
+       return res.status(400).send("id is not valid");
+     }
+
+     const problem = await Problem.findById(id);
+     if(!problem){
+      res.status(404).send("Problem is Missing")
+     }
+     res.status(200).send(problem)
+   } catch (err) {
+     res.status(500).send("Error :" + err);
+   }
+}
+
+const fetchallproblem = async (req, res)=>{
+  try {
+    const problems = await Problem.find({});
+    if (problems.length===0) {
+      res.status(404).send("Problems are Missing");
+    }
+    res.status(200).send(problems);
+  } catch (err) {
+    res.status(500).send("Error :" + err);
+  }
+}
+
+module.exports = {
+  createproblem,
+  updateproblem,
+  deleteproblem,
+  fetchproblem,
+  fetchallproblem,
+};
--- src/routes/problemCreator.js
+++ src/routes/problemCreator.js
@@ -2,14 +2,26 @@ const express = require("express");
 const Problemrouter = express.Router();
 const adminMiddleware = require("../middleware/adminMiddleware");
 const createproblem = require("../controllers/userProblems");
+const userMiddleware = require("../middleware/userMiddleware");
+const updateproblem = require("../controllers/userProblems");
+const deleteproblem = require("../controllers/userProblems");
+const fetchproblem = require("../controllers/userProblems");
+const fetchallproblem = require("../controllers/userProblems");
+const fetchallproblemsolved = require("../controllers/userProblems");
+
+
+
+
+
 
 
 Problemrouter.post('/create',adminMiddleware,createproblem);
-// Problemrouter.get('/:id',fetchproblem);
-// Problemrouter.get('/',fetchallproblem);
-// Problemrouter.patch('/:id',updateproblem);
-// Problemrouter.delete("/:id", deleteproblem);
-// Problemrouter.get("/user",fetchallproblemsolved);
+Problemrouter.patch("/update/:id", adminMiddleware,updateproblem);
+Problemrouter.delete("/delete/:id", adminMiddleware, deleteproblem);
+
+Problemrouter.get('/get/:id',userMiddleware, fetchproblem); 
+Problemrouter.get('/getall',userMiddleware,fetchallproblem);
+Problemrouter.get("/allproblems",userMiddleware,fetchallproblemsolved);
 
 
-module.exports = Problemrouter;;
+module.exports = Problemrouter;
