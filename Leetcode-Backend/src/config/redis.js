bda57da0540097db10cf967453ec47ea9408b1ed
priyanshh-09 <priyanshuchandola9@gmail.com>
2026-08-25 18:07:47 +0530

    Update Redis connection

--- Leetcode-Backend/src/config/redis.js
+++ Leetcode-Backend/src/config/redis.js
@@ -1,12 +1,32 @@
-const {createClient} = require("redis");
+// const {createClient} = require("redis");
+
+// const redisclient = createClient({
+//   username: "default",
+//   password: process.env.REDIS_PASS,
+//   socket: {
+//     host:"redis-16441.crce206.ap-south-1-1.ec2.cloud.redislabs.com",
+//     port:16441,
+//   },
+// });
+
+// module.exports = redisclient;
+
+
+
+const { createClient } = require("redis");
 
 const redisclient = createClient({
   username: "default",
   password: process.env.REDIS_PASS,
   socket: {
-    host: "redis-16441.crce206.ap-south-1-1.ec2.cloud.redislabs.com",
-    port: 16441,
+    host: process.env.REDIS_HOST,
+    port: Number(process.env.REDIS_PORT),
   },
 });
 
+redisclient.on("error", (err) => {
+  console.log("Redis Client Error", err);
+});
+
 module.exports = redisclient;
+
